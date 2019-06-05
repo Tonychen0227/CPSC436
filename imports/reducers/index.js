3751986ad353ce336c0eca29fac24743060eecbc
Tony Chen <tonyc@moj.io>
2019-06-04 21:23:11 -0700

    Add mongo connection str

--- imports/reducers/index.js
+++ imports/reducers/index.js
@@ -1,4 +1,13 @@
 import {combineReducers} from 'redux';
+const MongoClient = require('mongodb').MongoClient;
+
+const mongoConnectionString = "mongodb+srv://admin:@cpsc436-basketball-kbwxu.mongodb.net/test?retryWrites=true"
+
+MongoClient.connect(mongoConnectionString, function(err, client) {
+  console.log(err);
+  console.log("Connected successfully to server");
+  client.close();
+});
 
 const counterReducer = (count = 0, action) => {
   if (action.type === 'INCREMENT_COUNTER') {
