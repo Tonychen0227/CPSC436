b1f8d79dd8210fb4c2d19cc1249a8b8ecd30cbfe
Tony Chen <tonyc@moj.io>
2019-06-08 11:08:33 -0700

    Removed INCREMENT_COUNTER action

--- imports/actions/index.js
+++ imports/actions/index.js
@@ -1,11 +1,3 @@
-export const increment = amount => {
-  return {
-    //Payloads can be several, just cannot be duplicate names, think JSON
-    type: 'INCREMENT_COUNTER',
-    payload: amount
-  };
-};
-
 export const flipPage = newPage => {
   return {
     type: 'NEW_PAGE',
