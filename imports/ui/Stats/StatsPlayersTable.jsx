943969d0ad92a6cc73a43cdaf455adb9cb363264
Yuting Wen <ywen@paragon.internal>
2019-06-14 15:42:24 -0700

    STAT table: add some buttons so that we can display the cols we want

--- imports/ui/Stats/StatsPlayersTable.jsx
+++ imports/ui/Stats/StatsPlayersTable.jsx
@@ -10,77 +10,84 @@ class StatsPlayersTable extends React.Component {
 	constructor() {
     super();
     this.state = {
-      data: makeData()
+      data: makeData(),
+			columns: [
+	      {
+	        Header: "First Name",
+	        accessor: "firstName",
+					show: true,
+					width: 150
+	      },
+	      {
+	        Header: "Last Name",
+	        accessor: "lastName",
+					show: true,
+					width: 150
+	      },
+	      {
+	        Header: "Age",
+	        accessor: "age",
+					show: true,
+					width: 50
+	      },
+	      {
+	        Header: "2-Points-Attempt",
+	        accessor: "fg2PtAtt",
+					show: true,
+					width: 100
+	      },
+	      {
+	        Header: "2-Points-Attempt/Game",
+	        accessor: "fg2PtAttPerGame",
+					show: true,
+					width: 100
+	      },
+	      {
+	        Header: "2-Point-Made",
+	        accessor: "fg2PtMade",
+					show: true,
+					width: 100
+	      },
+	      {
+	        Header: "2-Point-Made/Game",
+	        accessor: "fg2PtMadePerGame",
+					show: true,
+					width: 100
+	      },
+	      {
+	        Header: "2-Point%",
+	        accessor: "fg2PtPct",
+					show: true,
+					width: 100
+	      }
+	    ]
     };
     this.tableScrollTop = 0;
+		this.displayCol = this.displayCol.bind(this);
   }
-  render() {
-    const { data } = this.state;
-    const fieldMap = ["firstName", "lastName", "age"];
 
-    const heads = ["First Name", "Last Name", "Age"];
-    const columns = [
-			{
-				Header: "Name",
-				columns: [
-		      {
-		        Header: "First Name",
-		        accessor: "firstName",
-						width: 150
-		      },
-		      {
-		        Header: "Last Name",
-		        accessor: "lastName",
-						width: 150
-		      }
-				]
-			},
-			{
-				Header: "Info",
-				columns: [
-		      {
-		        Header: "Age",
-		        accessor: "age",
-						width: 50
-		      }
-				]
-			},
-			{
-				Header: "Field Goals",
-				columns: [
-		      {
-		        Header: "2-Points-Attempt",
-		        accessor: "fg2PtAtt",
-						width: 100
-		      },
-		      {
-		        Header: "2-Points-Attempt/Game",
-		        accessor: "fg2PtAttPerGame",
-						width: 100
-		      },
-		      {
-		        Header: "2-Point-Made",
-		        accessor: "fg2PtMade",
-						width: 100
-		      },
-		      {
-		        Header: "2-Point-Made/Game",
-		        accessor: "fg2PtMadePerGame",
-						width: 100
-		      },
-		      {
-		        Header: "2-Point%",
-		        accessor: "fg2PtPct",
-						width: 100
-		      }
-				]
-			}
-    ];
+
+	displayCol = e => {
+		const cols = this.state.columns.map((col, i) => e===i? {...col, show: !col.show}: col)
+		this.setState({
+			columns: cols
+		});
+		console.log(this.state);
+	}
+
+  render() {
+    const { data } = this.state.data;
+
     return (
-      <div>
+			<div>
+				<div>
+					<button onClick={() => this.displayCol(0)}>A</button>
+					<button onClick={() => this.displayCol(1)}>B</button>
+					<button onClick={() => this.displayCol(2)}>C</button>
+				</div>
         <StatsPlayerDraggableTable
-          rows={data}
-          columns={columns}
+          rows={this.state.data}
+          columns={this.state.columns}
           defaultPageSize={20}
           filterable
           defaultFilterMethod={(filter, row) =>
