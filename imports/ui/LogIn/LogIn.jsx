64436d3876627a3f7134a4cbcaceb9f17748719f
Tony Chen <tonyc@moj.io>
2019-06-20 22:59:13 -0700

    Slight refactoring

--- imports/ui/LogIn/LogIn.jsx
+++ imports/ui/LogIn/LogIn.jsx
@@ -3,31 +3,14 @@ import Fragment from 'react';
 import UserDetails from './UserDetails';
 import NewsDashboard from './NewsDashboard';
 import PlayerStats from './PlayerStats';
+import MyAccount from './MyAccount';
 import MatchInformation from './MatchInformation';
 import '../../css/LogIn.css';
 
 class LogIn extends React.Component {
 	render() {
 		return (
-			<div className="main">
-				<h1> Welcome to your account page </h1>
-				<div className="left">
-					<div className="playerStats inner">
-						<PlayerStats/>
-					</div>
-					<div className="newsDashboard inner">
-						<NewsDashboard/>
-					</div>
-				</div>
-				<div className="right">
-					<div className="userDetails inner">
-						<UserDetails/>
-					</div>
-					<div className="matchInformation inner">
-						<MatchInformation/>
-					</div>
-				</div>
-			</div>
+			<MyAccount/>
 		);
 	};
 }
