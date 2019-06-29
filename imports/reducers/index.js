9644d9fbaff6a30badacfe09f0b92e18aa3f40c1
Tony Chen <tony.chen@outlook.com>
2019-06-29 11:18:22 -0700

    Save both URLs anyway

--- imports/reducers/index.js
+++ imports/reducers/index.js
@@ -3,8 +3,6 @@ import data from './data';
 var sha256 = require('js-sha256');
 const axios = require('axios');
 
-const apiUrl = "http://cpsc436basketballapi.herokuapp.com"
-
 const currentPageNumber = (pageNum = 1, action) => {
   if (action.type === 'NEW_PAGE') {
     return pageNum = action.payload;
