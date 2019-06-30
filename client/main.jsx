773b3e7a3726e466d32f1b7d38a9ec1e553139a1
Yuting Wen <blackalkane@gmail.com>
2019-06-30 15:09:15 -0700

    for stash change

--- client/main.jsx
+++ client/main.jsx
@@ -6,6 +6,8 @@ import App from '/imports/ui/Layout/App';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../imports/reducers';
 import thunk from 'redux-thunk';
+import "bootstrap/dist/css/bootstrap.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 Meteor.startup(() => {
   render(<Provider store={createStore(reducers, applyMiddleware(thunk))}>
