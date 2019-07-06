962b19ff46624a04b1f13d34567e3b2b443c9e31
Yuting Wen <blackalkane@gmail.com>
2019-07-05 21:56:02 -0700

    tiny link change

--- imports/ui/Stats/StatsNav.jsx
+++ imports/ui/Stats/StatsNav.jsx
@@ -10,11 +10,11 @@ class StatsNav extends React.Component {
       <nav className="nav-wrapper grey darken-3">
         <div className="container">
           <nav>
-            <Link to='/Stats/players'><button className="playersButton">Players</button></Link>
-            <Link to='/Stats/teams'><button className="teamsButton">Teams</button></Link>
+            <Link to='/stats/players'><button className="playersButton">Players</button></Link>
+            <Link to='/stats/teams'><button className="teamsButton">Teams</button></Link>
           </nav>
-          <Route exact path='/Stats/players' component={StatsPlayers} />
-          <Route exact path='/Stats/teams' component={StatsTeams} />
+          <Route exact path='/stats/players' component={StatsPlayers} />
+          <Route exact path='/stats/teams' component={StatsTeams} />
         </div>
       </nav>
     )
