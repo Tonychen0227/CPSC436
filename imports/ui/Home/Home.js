5efd1aaec9c5723145ac6e52a9ca881da6358487
peter.han@trulioo.com <peter.han@trulioo.com>
2019-07-25 16:33:51 -0700

    first commit

--- imports/ui/Home/Home.js
+++ imports/ui/Home/Home.js
@@ -3,23 +3,29 @@ import { connect } from 'react-redux';
 import '../../css/App.css';
 import '../../css/MyAccount.css';
 import NewsDashboard from '../LogIn/NewsDashboard'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
+
 class Home extends React.Component {
 	render() {
 		return (
 			<div className="Home">
-			<section className="hero is-link">
-			  <div className="hero-body">
-			    <div className="container">
-			      <h1 className="title">
-			        Home
+				<section className="hero is-link">
+					<div className="hero-body">
+						<div className="container">
+							<h1 className="title">
+								Home
 			      </h1>
-			    </div>
-			  </div>
-			</section>
+						</div>
+					</div>
+				</section>
 				<div className="center">
 					<div className="newsDashboard inner">
 						<NewsDashboard />
 					</div>
+				</div>
+				<div className="right box">
+					<TwitterTimelineEmbed sourceType="URL" url="https://twitter.com/NBA" options={{ height: 400 }} />
 				</div>
 			</div>
 		);
